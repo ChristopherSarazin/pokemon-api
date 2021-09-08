61e35351ec73842e4fb43417d198ba85abcff8ef
Christopher Sarazin <84635155+ChristopherSarazin@users.noreply.github.com>
2021-09-08 18:13:24 -0400

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Pokemon from './components/Pokemon';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Whos that Pokemon!</h1>
+      <Pokemon  />
+    </div>
+  );
+}
+
+export default App;
